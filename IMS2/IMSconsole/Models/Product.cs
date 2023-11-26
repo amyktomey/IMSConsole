c629c08065db0c818ce1eef368c02a606e1a64c0
Amy Tomey <amy.tomey59@gmail.com>
2023-11-26 18:02:01 -0500

    adding blozor elements, put in new appsettings.json (not sure if it's in the right place)added productsinventory.json to seed database... soon, hopefully

--- IMS2/IMSconsole/Models/Product.cs
+++ IMS2/IMSconsole/Models/Product.cs
@@ -9,7 +9,8 @@ public class Product
 	public string? Name { get; set; }
 
 	[Column(TypeName = "string(20)")]
-    public string ProductType { get; set; }
+    public string? ProductType { get; set; }
 	public decimal Price { get; set; }
 	public required string Description { get; set; }
+	public int Quantity { get; set; }	
 }
